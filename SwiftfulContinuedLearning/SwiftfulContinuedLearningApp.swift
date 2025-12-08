0a49de0e8b858a689d4a0a877fc530721e068756
Antonio <antonio.m.gargiulo@gmail.com>
2025-12-08 14:13:45 -0500

    Initial Commit

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulContinuedLearningApp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Antonio Gargiulo on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
